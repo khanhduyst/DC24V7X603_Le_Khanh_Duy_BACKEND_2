807798ac786fa5aec02bc1d5b546de81ffb237ed
DuystudentST <lkdffst@gmail.com>
2026-02-08 22:00:37 +0700

    Xong phan findByID

--- app/config/index.js
+++ app/config/index.js
@@ -2,5 +2,8 @@ const config = {
   app: {
     port: process.env.PORT || 3000,
   },
+  db: {
+    uri: process.env.MONGODB_URI || 'mongodb://localhost:27017/contactbook',
+  }
 };
 module.exports = config;
--- app/controllers/contact.controller.js
+++ app/controllers/contact.controller.js
@@ -1,27 +1,69 @@
-exports.create = (req, res) => {
-    res.send({ message: "Create handler" });
+const ContactService = require("../services/contact.service");
+const MongoDB = require("../utils/mongodb.util");
+const ApiError = require("../api-error");
+
+exports.create = async (req, res, next) => {
+  if (!req.body?.name) {
+    return next(new ApiError(400, "Name can not be empty"));
+  }
+
+  try {
+    const contactService = new ContactService(MongoDB.client);
+    const document = await contactService.create(req.body);
+    return res.send(document);
+  } catch (error) {
+    return next(
+      new ApiError(500, "An error occurred while creating the contact"),
+    );
+  }
 };
 
-exports.findAll = (req, res) => {
-    res.send({ message: "FindAll handler" });
+exports.findAll = async (req, res, next) => {
+  let documents = [];
+  try {
+    const contactService = new ContactService(MongoDB.client);
+    const { name } = req.query;
+
+    if (name) {
+      documents = await contactService.findByName(name);
+    } else {
+      documents = await contactService.find({});
+    }
+  } catch (error) {
+    return next(
+      new ApiError(500, "An error occurred while retrieving contacts"),
+    );
+  }
+  return res.send(documents);
 };
 
-exports.findOne = (req, res) => {
-    res.send({ message: "FindOne handler" });
+exports.findOne = async (req, res, next) => {
+  try {
+    const contactService = new ContactService(MongoDB.client);
+    const document = await contactService.findById(req.params.id);
+    if (!document) {
+      return next(new ApiError(404, "Contact not found"));
+    }
+    return res.send(document);
+  } catch (error) {
+    return next(
+      new ApiError(500, `Error retrieving contact with id=${req.params.id}`),
+    );
+  }
 };
 
 exports.update = (req, res) => {
-    res.send({ message: "Update handler" });
+  res.send({ message: "Update handler" });
 };
 
 exports.delete = (req, res) => {
-    res.send({ message: "Delete handler" });
+  res.send({ message: "Delete handler" });
 };
 
 exports.deleteAll = (req, res) => {
-    res.send({ message: "DeleteAll handler" });
+  res.send({ message: "DeleteAll handler" });
 };
 
 exports.findAllFavorite = (req, res) => {
-    res.send({ message: "FindAllFavorites handler" });
+  res.send({ message: "FindAllFavorites handler" });
 };
